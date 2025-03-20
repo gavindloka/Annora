0a1dddcd9a15b0c99a4fe9c10b4de8a0697c4579
gavindloka <lokagavindloka@gmail.com>
2025-03-20 21:40:52 +0700

    chore: revamp topbar

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -9,7 +9,7 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
       decoration: BoxDecoration(
         color: const Color(0xFFFDF6E3),
         borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20)),
@@ -24,18 +24,21 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 20),
+          SizedBox(height: 30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ClipOval(
-                child:Image.asset(
-                "assets/images/logo.png",
-                height: 40,
-                fit: BoxFit.contain,
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/logo.png"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
               ),
-              )
-              ,
               Stack(
                 children: [
                   GestureDetector(
@@ -47,7 +50,7 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
                     },
                     child: Icon(
                       Icons.notifications,
-                      size: 30,
+                      size: 35,
                       color: Colors.amber,
                     ),
                   ),
@@ -62,24 +65,29 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
               Row(
                 children: [
                   CircleAvatar(
-                    radius: 22,
-                    backgroundImage: AssetImage("assets/images/antony.jpeg"),
+                    radius: 25,
+                    backgroundColor: Colors.grey[200],
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
                   ),
-                  const SizedBox(width: 10),
+                  const SizedBox(width: 15),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         "Hai, $username",
                         style: const TextStyle(
-                          fontSize: 18,
+                          fontSize: 24,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       const Text(
                         "Selamat datang di Surveyor Annora",
                         style: TextStyle(
-                          fontSize: 14,
+                          fontSize: 16,
                           fontStyle: FontStyle.italic,
                         ),
                       ),
@@ -95,5 +103,5 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(120);
+  Size get preferredSize => const Size.fromHeight(170);
 }
